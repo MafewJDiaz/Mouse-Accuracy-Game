888e4b501cfb83a91d5dfa3ea5a5dd06ea9a9d74
adam-elalouani <adam_elalouani@horacemann.org>
2023-11-07 13:30:27 -0500

    adam

--- mainjs.js
+++ mainjs.js
@@ -12,13 +12,18 @@ document.querySelector("#close").addEventListener("click", function(){
 
 });
 
-var timeleft = 5;
+
+
+    var timeleft = 30;
 var downloadTimer = setInterval(function(){
     if(timeleft <= 0){
       clearInterval(downloadTimer);
     } else {
-        document.getElementById("countdown").innerHTML = timeleft + " seconds before the game starts.";
+        document.getElementById("countdown").innerHTML = timeleft + " until the game ends";
     }
     timeleft -= 1;
 }, 1000);
 
+document.getElementById("circle").onclick = function() {
+    document.getElementById("circle").style.display ="none";
+}
