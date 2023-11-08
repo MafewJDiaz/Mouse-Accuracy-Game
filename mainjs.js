7f5ded8564d5f741142f62fd9b562b50e66c7086
adam-elalouani <adam_elalouani@horacemann.org>
2023-11-08 13:19:46 -0500

    adam

--- mainjs.js
+++ mainjs.js
@@ -5,122 +5,73 @@ window.addEventListener("load", function () {
   }, 1500);
 });
 
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-    console.log("close");
-    
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+  document.querySelector(".countdown").style.display = "block";
+  startGame();
 });
 
+let clickCount = 0;
+let circleClickCount = 0;
+let animationCount = 0;
 
+function startGame() {
+  const countdown = document.getElementById('safeTimerDisplay');
+  let sec = 3;
+  const timer = setInterval(function () {
+      countdown.innerHTML = sec;
+      sec--;
 
+      if (sec < -1) {
+          clearInterval(timer);
+          document.querySelector(".countdown").style.display = "none";
+          createAndAnimateCircle();
+      }
+  }, 1000);
+}
 
+function createAndAnimateCircle() {
+  if (animationCount < 15) {
+    const circle = document.querySelector("#circle");
+    circle.style.display = "block";
+    circle.style.width = "0"; // Start from 0 width
+    circle.style.height = "0"; // Start from 0 height
+    circle.style.opacity = "1";
 
+    const x = getRandomLocation(0, window.innerWidth - 400); // Ensure it fits within the window
+    const y = getRandomLocation(0, window.innerHeight - 400); // Ensure it fits within the window
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
 
-    var timeleft = 30;
-var downloadTimer = setInterval(function(){
-    if(timeleft <= 0){
-      clearInterval(downloadTimer);
-    } else {
-        document.getElementById("countdown").innerHTML = timeleft + " until the game ends";
-    }
-    timeleft -= 1;
-}, 1000);
+    circle.addEventListener("click", function () {
+      if (parseFloat(circle.style.width) > 0) {
+        circleClickCount+;
+        document.querySelector(".scorebar ul").innerHTML += "<li class='dot'></li>";
+        document.querySelector(".circle").style.display= "none"; // Fully disappear when clicked
+      }
+    });
 
-document.getElementById("circle").onclick = function() {
-    document.getElementById("circle").style.display ="none";
+    const animationInterval = setInterval(function () {
+      if (parseFloat(circle.style.width) < 400) {
+        // Increase the width and height to 400
+        circle.style.width = (parseFloat(circle.style.width) + 4) + "px";
+        circle.style.height = (parseFloat(circle.style.height) + 4) + "px";
+      } else {
+        clearInterval(animationInterval);
+        // After reaching 400, hide the circle
+        circle.style.opacity = "0";
+
+        setTimeout(function () {
+          createAndAnimateCircle();
+        }, 500);
+      }
+    }, 25);
+  }
 }
 
 
-const possible = document.querySelector(".possible");
-
-function randomcircles() {
-        const target = doucment.createElement("div");
-        target.className = "target";
-        const randomSize = Math.floor (Math.random() * 50)+25;
-        const randomX = Math.floor(Math.random()*(possible.offsetWidth - randomSize));
-        const randomY = Math.floor(Math.random()*(possible.offsetHeight - randomSize) );
-       
-        possible.appendChild(target);
-
-        
-            setTimeout ( () =>{
-                target.style.transform = "scale(1)";
-                target.style.opacity = 1;
-        
-            });
-
-                setTimeout ( () => {
-                    target.style.transform = "scale(0)"
-                    target.style.opacity = 0;
-                    target.addEventListener ("click", () => { 
-                        possible.removeChild(target);
-                });
-
-                 },5000);
-},5);
-
-target.addEventListener("disappear",() => {
-        possible.removeChild(target);
-            } );
-        
-            const time = 30000;
-            const targetduration = 2000;
-            let gameTimer = setInterval(randomcircles, targetduration);
 
 
-
-
-
-    var timeleft = 30;
-var downloadTimer = setInterval(function(){
-    if(timeleft <= 0){
-      clearInterval(downloadTimer);
-    } else {
-        document.getElementById("countdown").innerHTML = timeleft + " until the game ends";
-    }
-    timeleft -= 1;
-}, 1000);
-
-document.getElementById("circle").onclick = function() {
-    document.getElementById("circle").style.display ="none";
+function getRandomLocation(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-const possible = document.querySelector(".possible");
-
-function randomcircles() {
-        const target = doucment.createElement("div");
-        target.className = "target";
-        const randomSize = Math.floor (Math.random() * 50)+25;
-        const randomX = Math.floor(Math.random()*(possible.offsetWidth - randomSize));
-        const randomY = Math.floor(Math.random()*(possible.offsetHeight - randomSize) );
-       
-        possible.appendChild(target);
-
-        
-            setTimeout ( () =>{
-                target.style.transform = "scale(1)";
-                target.style.opacity = 1;
-        
-            });
-
-                setTimeout ( () => {
-                    target.style.transform = "scale(0)"
-                    target.style.opacity = 0;
-                    target.addEventListener ("click", () => { 
-                        possible.removeChild(target);
-                });
-
-                 },5000);
-},5);
-
-target.addEventListener("disappear",() => {
-        possible.removeChild(target);
-            } );
-        
-            const time = 30000;
-            const targetduration = 2000;
-            let gameTimer = setInterval(randomcircles, targetduration);
-
-
-    
