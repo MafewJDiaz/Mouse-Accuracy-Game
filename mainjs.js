8ccf1003fc51797b5b2ee15f5171a32926c7fc83
adam-elalouani <adam_elalouani@horacemann.org>
2023-11-07 22:11:14 -0500

    adam

--- mainjs.js
+++ mainjs.js
@@ -27,3 +27,44 @@ var downloadTimer = setInterval(function(){
 document.getElementById("circle").onclick = function() {
     document.getElementById("circle").style.display ="none";
 }
+
+
+const possible = document.querySelector("possible");
+function randomcircles() {
+        const target = doucment.createElement("div");
+        target.className = "target";
+        const randomSize = Math.floor (Math.random() * 50)+25;
+        const randomX = Math.floor(Math.random()*(possible.offsetWidth - randomSize));
+        const randomY = Math.floor(Math.random()*(possible.offsetHeight - randomSize) );
+
+        possible.appendChild(target);
+
+        
+            target.style.transform = "scale(1)";
+            target.style.opacity = 1;
+        
+            setTimeout ( () =>{
+                target.style.transform = "scale(1)";
+                target.style.opacity = 1;
+        
+            },1000);
+
+            setTimeout ( () => {
+                target.style.transform = "scale(0)"
+                target.style.opacity = 0;
+                target.addEventListener ("click", () => { 
+                    possible.removeChild(target);
+                }
+                );
+            },5000);
+
+            target.addEventListener("disappear",() => {
+                possible.removeChild(target);
+            } );
+        
+            const time = 30000;
+            const targetduration = 1000;
+            let gameTimer = setInterval(randomcircles, targetduration);
+            
+    }
+    
